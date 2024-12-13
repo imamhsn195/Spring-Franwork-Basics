898ea45853d4d4a19ccee00dd85db86124c42de1
imamhsn195 <imamhsn195@gmail.com>
2024-12-14 02:06:35 +0400

    Add object as Dependancy Injection

--- src/main/java/org/example/Triangle.java
+++ src/main/java/org/example/Triangle.java
@@ -1,33 +1,37 @@
 package org.example;
 
 public class Triangle {
-
-    public Triangle(int height){
-        this.height = height;
-    }
-    public Triangle(String type){
-        this.type = type;
-    }
-    public  Triangle(String type, int height){
-        this.type = type;
-        this.height = height;
-    }
-    private String type;
-    private int height;
-
-    public int getHeight() {
-        return height;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
-
+    private Point pointA;
+    private Point pointB;
+    private Point pointC;
     public void draw(){
-        System.out.println(type + " drawing triangle shape of height " + height);
+        System.out.println("Point A = ( " + pointA.getX() + ", " + pointA.getY() + " )");
+        System.out.println("Point B = ( " + pointB.getX() + ", " + pointB.getY() + " )");
+        System.out.println("Point C = ( " + pointC.getX() + ", " + pointC.getY() + " )");
     }
+
+    public Point getPointA() {
+        return pointA;
+    }
+
+    public void setPointA(Point pointA) {
+        this.pointA = pointA;
+    }
+
+    public Point getPointB() {
+        return pointB;
+    }
+
+    public void setPointB(Point pointB) {
+        this.pointB = pointB;
+    }
+
+    public Point getPointC() {
+        return pointC;
+    }
+
+    public void setPointC(Point pointC) {
+        this.pointC = pointC;
+    }
+
 }
