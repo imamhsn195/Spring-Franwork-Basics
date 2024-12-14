416aec28f9b96d76a25d9e8ac34f14be7fb286de
imamhsn195 <imamhsn195@gmail.com>
2024-12-14 12:43:57 +0400

    Spring autowiring byName, byType, constructor

--- src/main/java/org/example/Triangle.java
+++ src/main/java/org/example/Triangle.java
@@ -1,20 +1,43 @@
 package org.example;
 
-import java.util.List;
-
 public class Triangle {
-    public List<Point> getPoints() {
-        return points;
+    public Triangle(Point pointA, Point pointB, Point pointC) {
+        this.pointA = pointA;
+        this.pointB = pointB;
+        this.pointC = pointC;
     }
 
-    public void setPoints(List<Point> points) {
-        this.points = points;
-    }
-
-    private List<Point> points;
+    private Point pointA;
+    private Point pointB;
+    private Point pointC;
     public void draw(){
-        for(Point point: points){
-            System.out.println("Point = ( " + point.getX() + ", " + point.getY() + " )");
-        }
+        System.out.println("Point A = ( " + pointA.getX() + ", " + pointA.getY() + " )");
+        System.out.println("Point B = ( " + pointB.getX() + ", " + pointB.getY() + " )");
+        System.out.println("Point C = ( " + pointC.getX() + ", " + pointC.getY() + " )");
     }
+
+    public Point getPointA() {
+        return pointA;
+    }
+
+    public void setPointA(Point pointA) {
+        this.pointA = pointA;
+    }
+
+    public Point getPointB() {
+        return pointB;
+    }
+
+    public void setPointB(Point pointB) {
+        this.pointB = pointB;
+    }
+
+    public Point getPointC() {
+        return pointC;
+    }
+
+    public void setPointC(Point pointC) {
+        this.pointC = pointC;
+    }
+
 }
