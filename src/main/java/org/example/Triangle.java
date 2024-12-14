d9b95e9cdad8924357dc03a188ddbd9a55f8bf52
imamhsn195 <imamhsn195@gmail.com>
2024-12-14 12:33:27 +0400

    Implement list variable

--- src/main/java/org/example/Triangle.java
+++ src/main/java/org/example/Triangle.java
@@ -1,37 +1,20 @@
 package org.example;
 
+import java.util.List;
+
 public class Triangle {
-    private Point pointA;
-    private Point pointB;
-    private Point pointC;
+    public List<Point> getPoints() {
+        return points;
+    }
+
+    public void setPoints(List<Point> points) {
+        this.points = points;
+    }
+
+    private List<Point> points;
     public void draw(){
-        System.out.println("Point A = ( " + pointA.getX() + ", " + pointA.getY() + " )");
-        System.out.println("Point B = ( " + pointB.getX() + ", " + pointB.getY() + " )");
-        System.out.println("Point C = ( " + pointC.getX() + ", " + pointC.getY() + " )");
+        for(Point point: points){
+            System.out.println("Point = ( " + point.getX() + ", " + point.getY() + " )");
+        }
     }
-
-    public Point getPointA() {
-        return pointA;
-    }
-
-    public void setPointA(Point pointA) {
-        this.pointA = pointA;
-    }
-
-    public Point getPointB() {
-        return pointB;
-    }
-
-    public void setPointB(Point pointB) {
-        this.pointB = pointB;
-    }
-
-    public Point getPointC() {
-        return pointC;
-    }
-
-    public void setPointC(Point pointC) {
-        this.pointC = pointC;
-    }
-
 }
