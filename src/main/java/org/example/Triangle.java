a5c6e287b4ef706b7d99a33ede8bf694c79c5eb3
imamhsn195 <imamhsn195@gmail.com>
2024-12-21 00:28:27 +0400

    overwrite list properties and merge list elements

--- src/main/java/org/example/Triangle.java
+++ src/main/java/org/example/Triangle.java
@@ -5,41 +5,26 @@ import org.springframework.beans.factory.BeanNameAware;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 
+import java.util.List;
+
 public class Triangle implements ApplicationContextAware, BeanNameAware {
-    private Point pointA;
-    private Point pointB;
-    private Point pointC;
+    public List<Point> getPoints() {
+        return points;
+    }
+
+    public void setPoints(List<Point> points) {
+        this.points = points;
+    }
+
+    private List<Point> points;
     private ApplicationContext context;
 
     public void draw(){
-        System.out.println("Point A = ( " + pointA.getX() + ", " + pointA.getY() + " )");
-        System.out.println("Point B = ( " + pointB.getX() + ", " + pointB.getY() + " )");
-        System.out.println("Point C = ( " + pointC.getX() + ", " + pointC.getY() + " )");
+        for (Point point: points) {
+            System.out.println("Point = ( " + point.getX() + ", " + point.getY() + " )");
+        }
     }
 
-    public Point getPointA() {
-        return pointA;
-    }
-
-    public void setPointA(Point pointA) {
-        this.pointA = pointA;
-    }
-
-    public Point getPointB() {
-        return pointB;
-    }
-
-    public void setPointB(Point pointB) {
-        this.pointB = pointB;
-    }
-
-    public Point getPointC() {
-        return pointC;
-    }
-
-    public void setPointC(Point pointC) {
-        this.pointC = pointC;
-    }
 
     @Override
     public void setBeanName(String beanName) {
