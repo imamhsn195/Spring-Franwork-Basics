63bddc3c710d45ed7db0cbcb7b649131f0fafd28
imamhsn195 <imamhsn195@gmail.com>
2024-12-21 01:43:44 +0400

    AbstractApplicationContext, context.registerShutDownHook, Initialization, Disposible interfaces,  init-method, destroy-method, default-init-method, default-destroy-method

--- src/main/java/org/example/DrawingApp.java
+++ src/main/java/org/example/DrawingApp.java
@@ -1,14 +1,14 @@
 package org.example;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class DrawingApp {
     public static void main(String[] args) {
-//        Triangle triangle  = new Triangle();
-//        BeanFactory beanFactory = new XmlBeanFactory(new FileSystemResource("src/main/resources/spring-config.xml"));
-        ApplicationContext context = new ClassPathXmlApplicationContext("spring-config.xml");
-        Triangle triangle = (Triangle) context.getBean("Triangle1");
+        AbstractApplicationContext context = new ClassPathXmlApplicationContext("spring-config.xml");
+        context.registerShutdownHook();
+        Triangle triangle = (Triangle) context.getBean("Triangle");
         triangle.draw();
     }
 }
--- src/main/java/org/example/Triangle.java
+++ src/main/java/org/example/Triangle.java
@@ -1,38 +1,56 @@
 package org.example;
 
-import org.springframework.beans.BeansException;
-import org.springframework.beans.factory.BeanNameAware;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
 
-import java.util.List;
-
-public class Triangle implements ApplicationContextAware, BeanNameAware {
-    public List<Point> getPoints() {
-        return points;
-    }
-
-    public void setPoints(List<Point> points) {
-        this.points = points;
-    }
-
-    private List<Point> points;
-    private ApplicationContext context;
-
+public class Triangle implements InitializingBean, DisposableBean {
+//public class Triangle{
+    private Point pointA;
+    private Point pointB;
+    private Point pointC;
     public void draw(){
-        for (Point point: points) {
-            System.out.println("Point = ( " + point.getX() + ", " + point.getY() + " )");
-        }
+        System.out.println("Point A = ( " + pointA.getX() + ", " + pointA.getY() + " )");
+        System.out.println("Point B = ( " + pointB.getX() + ", " + pointB.getY() + " )");
+        System.out.println("Point C = ( " + pointC.getX() + ", " + pointC.getY() + " )");
     }
 
+    public Point getPointA() {
+        return pointA;
+    }
 
-    @Override
-    public void setBeanName(String beanName) {
-        System.out.println("Bean Name: " + beanName);
+    public void setPointA(Point pointA) {
+        this.pointA = pointA;
+    }
+
+    public Point getPointB() {
+        return pointB;
+    }
+
+    public void setPointB(Point pointB) {
+        this.pointB = pointB;
+    }
+
+    public Point getPointC() {
+        return pointC;
+    }
+
+    public void setPointC(Point pointC) {
+        this.pointC = pointC;
     }
 
     @Override
-    public void setApplicationContext(ApplicationContext context) throws BeansException {
-        this.context = context;
+    public void destroy() throws Exception {
+        System.out.println("Destroy method is called");
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("Initialisation method is called");
+    }
+    public void my_init(){
+        System.out.println("my_init method is called.");
+    }
+    public void clean_up(){
+        System.out.println("clean_up method is called.");
     }
 }
