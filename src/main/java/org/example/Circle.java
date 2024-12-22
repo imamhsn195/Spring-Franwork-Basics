9100febc8d3df01da554b461711b6638fcda7e37
imamhsn195 <imamhsn195@gmail.com>
2024-12-22 22:38:03 +0400

    using messagesource to get text from properties

--- src/main/java/org/example/Circle.java
+++ src/main/java/org/example/Circle.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Service;
@@ -21,9 +23,21 @@ public class Circle implements Shape{
 
     private Point center;
 
+    public MessageSource getMessageSource() {
+        return messageSource;
+    }
+
+    @Autowired
+    public void setMessageSource(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
+
+    private MessageSource messageSource;
+
     @Override
     public void draw() {
-        System.out.println("Drawing circle with point (" + center.getX() + ", " + center.getY() + ")");
+        System.out.println(messageSource.getMessage("drawing.circle", null, "Message from Circle draw method", null));
+        System.out.println(messageSource.getMessage("drawing.point", new Object[] { center.getX(), center.getY()}, "Drawing circle point default message", null));
     }
     @PostConstruct
     public void initializeCircle(){
