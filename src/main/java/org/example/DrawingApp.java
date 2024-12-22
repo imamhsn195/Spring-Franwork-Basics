6d57ac0b2402a6093e49bcc469d4c822d968fb1b
imamhsn195 <imamhsn195@gmail.com>
2024-12-22 16:49:16 +0400

    Component and other Stereotype annotations

--- src/main/java/org/example/DrawingApp.java
+++ src/main/java/org/example/DrawingApp.java
@@ -1,6 +1,5 @@
 package org.example;
 
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -8,7 +7,7 @@ public class DrawingApp {
     public static void main(String[] args) {
         AbstractApplicationContext context = new ClassPathXmlApplicationContext("spring-config.xml");
         context.registerShutdownHook();
-        Shape shape = (Shape) context.getBean("Circle");
+        Shape shape = (Shape) context.getBean("circle");
         shape.draw();
     }
 }
